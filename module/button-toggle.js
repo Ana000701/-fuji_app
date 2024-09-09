b8e9b3467729d8fb0cdc8d0e0bb937d7cfc5001a
Ana <anawu.datawork@gmail.com>
2024-09-09 23:05:52 +0800

    update js ver.2

--- module/button-toggle.js
+++ module/button-toggle.js
@@ -2,6 +2,7 @@
 const categoryName = document.querySelector(".category-name");
 const categoryItem = document.querySelectorAll(".category-item");
 const modal = document.querySelectorAll(".modal");
+const body = document.querySelector("body");
 
 categoryItem.forEach((item) => {
   item.addEventListener("click", () => {
@@ -11,27 +12,52 @@ categoryItem.forEach((item) => {
     modal[0].style.display = "none";
     const modalBackdrop = document.querySelector(".modal-backdrop");
     modalBackdrop.remove();
+    body.classList.remove("modal-open");
+    body.style = "";
   });
 });
 
 // partner
-const partnerSelectMame = document.querySelector(".partner-select-name");
+const partnerBtn = document.querySelector(".partner-btn");
+const partnerSelectName = document.querySelector(".partner-select-name");
 const partnerItem = document.querySelectorAll(".partner-item");
+const partner2Item = document.querySelectorAll(".partner2-item");
 const partnerName = document.querySelector(".partner-name");
 const partnerImage = document.querySelector(".partner-image");
-console.log(partnerName);
-console.log(partnerItem[0].innerText);
-console.log(partnerImage.src);
 
 partnerItem.forEach((item) => {
   item.addEventListener("click", () => {
-    partnerSelectMame.innerText = item.children[1].innerText;
+    partnerSelectName.innerText = item.children[1].innerText;
     partnerName.innerText = item.children[1].innerText;
     partnerImage.src = `../public/images/${partnerName.innerText}.svg`;
     modal[1].classList.remove("show");
     modal[1].style.display = "none";
+    partnerBtn.style.display = "none";
+    partnerCollapse.style.display = "block";
     const modalBackdrop = document.querySelector(".modal-backdrop");
     modalBackdrop.remove();
+    body.classList.remove("modal-open");
+    body.style = "";
+  });
+});
+
+const collapseCancelBtn = document.querySelector(".collapse-cancel-button");
+const partnerCollapse = document.querySelector(".partner-collapse");
+collapseCancelBtn.addEventListener("click", () => {
+  partnerCollapse.style.display = "none";
+  partnerBtn.style.display = "block";
+});
+partner2Item.forEach((item) => {
+  item.addEventListener("click", () => {
+    partnerSelectName.innerText = item.children[1].innerText;
+    partnerName.innerText = item.children[1].innerText;
+    partnerImage.src = `../public/images/${partnerName.innerText}.svg`;
+    modal[2].classList.remove("show");
+    modal[2].style.display = "none";
+    const modalBackdrop = document.querySelector(".modal-backdrop");
+    modalBackdrop.remove();
+    body.classList.remove("modal-open");
+    body.style = "";
   });
 });
 
