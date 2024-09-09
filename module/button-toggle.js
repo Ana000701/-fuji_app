20ff87480771c3404526d52b32c67eb073745559
Ana <anawu.datawork@gmail.com>
2024-09-09 21:38:26 +0800

    js ver.1

--- module/button-toggle.js
+++ module/button-toggle.js
@@ -1,3 +1,41 @@
+// category
+const categoryName = document.querySelector(".category-name");
+const categoryItem = document.querySelectorAll(".category-item");
+const modal = document.querySelectorAll(".modal");
+
+categoryItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    categoryName.innerText = item.children[1].innerText;
+    categoryName.style.color = "#000";
+    modal[0].classList.remove("show");
+    modal[0].style.display = "none";
+    const modalBackdrop = document.querySelector(".modal-backdrop");
+    modalBackdrop.remove();
+  });
+});
+
+// partner
+const partnerSelectMame = document.querySelector(".partner-select-name");
+const partnerItem = document.querySelectorAll(".partner-item");
+const partnerName = document.querySelector(".partner-name");
+const partnerImage = document.querySelector(".partner-image");
+console.log(partnerName);
+console.log(partnerItem[0].innerText);
+console.log(partnerImage.src);
+
+partnerItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    partnerSelectMame.innerText = item.children[1].innerText;
+    partnerName.innerText = item.children[1].innerText;
+    partnerImage.src = `../public/images/${partnerName.innerText}.svg`;
+    modal[1].classList.remove("show");
+    modal[1].style.display = "none";
+    const modalBackdrop = document.querySelector(".modal-backdrop");
+    modalBackdrop.remove();
+  });
+});
+
+// 重複扣款、自動還款提醒展開效果
 const repeatPaymentBtn = document.querySelector("#repeat-payment-btn");
 const repeatPayment = document.querySelector(".repeat-payment");
 
